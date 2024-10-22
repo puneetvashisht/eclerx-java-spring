b5982ef2e3a160ef5d95417fac6df493b943e277
Puneet Vashisht <puneetvashisht@gmail.com>
2024-10-22 11:51:43 +0530

    final arrays

--- com/eclerx/day1/ArraysDemo.java
+++ com/eclerx/day1/ArraysDemo.java
@@ -1,19 +1,32 @@
 package com.eclerx.day1;
 
+import java.util.Arrays;
+
 public class ArraysDemo {
     public static void main(String[] args) {
         // int student1Score = 34;
 
         int scores[] = {33,56,776,23,2,232,1,121,211,11};
-        System.out.println(scores[scores.length - 1]);
+        // System.out.println(scores[scores.length - 1]);
 
         // for(int i = 0; i <= scores.length ; i++){
         //     System.out.println(scores[i]);
         // }
 
         // enhanced for loop
+        int totalScore = 0;
+        // int maxScore = 0;
+       
+
+        Arrays.sort(scores);
+        System.out.println(scores);
+
+
         for(int score: scores){
             System.out.println(score);
+            totalScore += score;
         }
+        System.out.println("Max Score" + scores[scores.length -1 ]);
+        System.out.println("Average " + (totalScore/scores.length));
     }
 }
