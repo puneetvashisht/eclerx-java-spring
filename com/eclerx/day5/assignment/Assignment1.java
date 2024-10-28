c0f0c071716dd6e37623335ad03d0df175ab8002
puneetvashisht <puneetvashisht@gmail.com>
2024-10-28 12:02:04 +0530

    final commit day5

--- com/eclerx/day5/assignment/Assignment1.java
+++ com/eclerx/day5/assignment/Assignment1.java
@@ -1,6 +1,7 @@
 package com.eclerx.day5.assignment;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,20 +15,25 @@ public class Assignment1 {
         fruits.add("banana");
 
 
-        String searchName = "pineapple";
-          Optional<String> foundName = fruits.stream()
-                                           .filter(name -> name.equalsIgnoreCase(searchName))
-                                           .findFirst();
-
-        // Handle the Optional value
-        foundName.ifPresentOrElse(
-            name -> System.out.println("Found name: " + name), // if present
-            () -> System.out.println("Name not found: " + searchName) // if absent
-        );
+        fruits.stream()
+        .sorted(Comparator.comparingInt(String::length))
+        .forEach(System.out::println);
 
 
-        String result = foundName.orElse("default");
-        System.out.println(result);
+        // String searchName = "pineapple";
+        //   Optional<String> foundName = fruits.stream()
+        //                                    .filter(name -> name.equalsIgnoreCase(searchName))
+        //                                    .findFirst();
+
+        // // Handle the Optional value
+        // foundName.ifPresentOrElse(
+        //     name -> System.out.println("Found name: " + name), // if present
+        //     () -> System.out.println("Name not found: " + searchName) // if absent
+        // );
+
+
+        // String result = foundName.orElse("default");
+        // System.out.println(result);
 
 
 
